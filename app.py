7e497f75c5228900748b6acaf226b7db21ef05f3
fjar86github <84884351+fjar86github@users.noreply.github.com>
2026-02-26 16:54:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import ollama
 import os
+#Tambahkan di app.py
+from functools import wraps
+
 
 app = Flask(__name__)
 app.secret_key = "sekolah-secret"
@@ -13,6 +16,31 @@ embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L
 db = Chroma(persist_directory="db", embedding_function=embedding)
 
 memory_store = {}
+
+#Tambahkan USER & PASSWORD:
+ADMIN_USER = "admin"
+ADMIN_PASS = "sekolah123"
+#Fungsi proteksi login
+def login_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if not session.get("admin"):
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated
+
+#Route Login
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        u = request.form["username"]
+        p = request.form["password"]
+
+        if u == ADMIN_USER and p == ADMIN_PASS:
+            session["admin"] = True
+            return redirect("/admin")
+
+    return render_template("login.html")
 
 def add_pdf_to_db(filepath):
     loader = PyPDFLoader(filepath)
@@ -74,10 +102,12 @@ def chat():
     return jsonify({"answer": answer})
 
 @app.route("/admin")
+@login_required
 def admin():
     return render_template("admin.html")
 
 @app.route("/upload", methods=["POST"])
+@login_required
 def upload():
     file = request.files["file"]
     path = os.path.join("docs", file.filename)
